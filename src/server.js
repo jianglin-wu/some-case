a67716a65e70e2c63efb07ccae5f99ed3d172097
jianglin-wu <wjiangl.id@gmail.com>
2019-09-25 17:13:33 +0800

    feat: 实现服务端路由直出

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 import path from 'path';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom';
 import Koa from 'koa';
 import route from 'koa-route';
 import serve from 'koa-static';
 import prettier from 'prettier';
-import Home from './pages/index.js';
+import App from './pages';
 
-const usePrettier = false;
+const usePrettier = !false;
 const app = new Koa();
 const genHtml = contentStr => {
   return `
@@ -26,8 +27,12 @@ const genHtml = contentStr => {
 };
 
 const main = ctx => {
-  console.log(ctx.request.url);
-  let htmlContent = ReactDOMServer.renderToString(<Home />);
+  const context = {};
+  let htmlContent = ReactDOMServer.renderToString(
+    <StaticRouter location={ctx.request.url} context={context}>
+      <App />
+    </StaticRouter>,
+  );
   htmlContent = genHtml(htmlContent);
   if (usePrettier) {
     htmlContent = prettier.format(htmlContent, { parser: 'html' });
@@ -42,13 +47,19 @@ app.use(
   }),
 );
 
+// SSR
+app.use(route.get('/', main));
+
 // SPA 静态文件
 app.use(serve(path.resolve(__dirname, '../dist')), {
+  index: '',
   extensions: ['.js', '.css', '.png', '.jpeg', '.jpg', 'gif'],
 });
 
 // SSR
 app.use(route.get('/*', main));
+
+
 
 app.listen(3000, () => {
   console.log('http://localhost:3000');
