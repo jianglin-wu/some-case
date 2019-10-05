bfc60d8dbfbf43772b028a7c56c9573e4c26d2ad
jianglin-wu <wjiangl.id@gmail.com>
2019-10-05 14:07:55 +0800

    feat: 使用 register-service-worker

--- src/client.js
+++ src/client.js
@@ -3,23 +3,45 @@ import ReactDOM from 'react-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { Provider as ReduxProvider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { register } from 'register-service-worker';
 import createStore from '@/store';
 import App from '@/pages';
-import * as serviceWorker from '@/sw-register';
 
-serviceWorker.register({
-  onUpdate: () => {
+register('/sw.js', {
+  registrationOptions: { scope: './' },
+  ready() {
+    // eslint-disable-next-line no-console
+    console.log('[register-service-worker] Service worker is active.');
+  },
+  registered() {
+    // eslint-disable-next-line no-console
+    console.log('[register-service-worker] Service worker has been registered.');
+  },
+  cached() {
+    // eslint-disable-next-line no-console
+    console.log('[register-service-worker] Content has been cached for offline use.');
+  },
+  updatefound() {
+    // eslint-disable-next-line no-console
+    console.log('[register-service-worker] New content is downloading.');
+  },
+  updated() {
     // 弹出提示，引导用户刷新页面
     // eslint-disable-next-line
-    const status = window.confirm('更新 Server Worker ？');
-    if (!status) {
+    if (!window.confirm('[register-service-worker] Server Worker 更新完成，是否立即刷新页面？')) {
       return;
     }
     window.location.reload();
   },
-  onSuccess: () => {
-    // eslint-disable-next-line
-    alert('更新成功');
+  offline() {
+    // eslint-disable-next-line no-console
+    console.log(
+      '[register-service-worker] No internet connection found. App is running in offline mode.',
+    );
+  },
+  error(error) {
+    // eslint-disable-next-line no-console
+    console.error('[register-service-worker] Error during service worker registration:', error);
   },
 });
 
@@ -48,4 +70,8 @@ if (module.hot) {
   module.hot.accept('./pages', () => {
     render();
   });
+}
+
+if (!navigator.onLine) {
+  console.log('offline');
 }
--- src/pages/Fetch/index.js
+++ src/pages/Fetch/index.js
@@ -19,7 +19,7 @@ const HelmetComponent = () => {
   );
 };
 
-@BasicLayout({ HelmetComponent, title: 'Fetch Data2' })
+@BasicLayout({ HelmetComponent, title: 'Fetch Data' })
 @connect(
   ({ posts: { list } }) => ({ postsList: list }),
   dispatchConnect,
