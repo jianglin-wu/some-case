dffddc0313cfd48ab74a8e1ea19a36884849dfb2
jianglin-wu <wjiangl.id@gmail.com>
2019-10-06 15:29:15 +0800

    perf: registerNavigationRoute

--- src/sw.js
+++ src/sw.js
@@ -20,16 +20,17 @@ workbox.core.skipWaiting();
 // eslint-disable-next-line
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
 
-// workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL('/index.html'), {
-//   blacklist: [/^\/_/, /\/[^/]+\.[^/]+$/],
-// });
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL('/index.html'), {
+  whitelist: [/^((?!\.).)*$/],
+  // blacklist: [/^\/_/, /\/[^/]+\.[^/]+$/],
+  // blacklist: [/\.(js|css|jpe?g|png)$/i],
+});
 
 workbox.routing.registerRoute(
   ({ url }) => {
     // eslint-disable-next-line no-restricted-globals
     return !!(url.origin === self.location.origin && /^((?!\.).)*$/.test(url.pathname));
   },
-  // /^((?!\.).)*$/,
   new workbox.strategies.NetworkFirst({
     plugins: [
       new workbox.cacheableResponse.Plugin({
