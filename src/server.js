032f5b551695907de3c3322ab207812b6e266c60
jianglin-wu <wjiangl.id@gmail.com>
2019-10-08 13:49:59 +0800

    feat: add node moniter

--- src/server.js
+++ src/server.js
@@ -2,10 +2,13 @@ import path from 'path';
 import Koa from 'koa';
 import route from 'koa-route';
 import serve from 'koa-static';
+import swStats from 'swagger-stats';
+import e2k from 'express-to-koa';
 import { matchPath } from 'react-router-dom';
 import render from '@/ssr/render';
 import createStore from '@/store';
 import routes from '@/pages/routes';
+import pkg from '../package.json';
 
 // request https 不检查证书
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
@@ -23,6 +26,15 @@ const main = async ctx => {
   await Promise.all(dataRequirements);
   ctx.response.body = render({ url, query }, store);
 };
+
+app.use(
+  e2k(
+    swStats.getMiddleware({
+      name: pkg.name,
+      version: pkg.version,
+    }),
+  ),
+);
 
 // 监控状态
 app.use(
