699efc7eb932232e931642455ba16b6d5e278bb5
jianglin-wu <wjiangl.id@gmail.com>
2019-10-23 13:31:21 +0800

    style: prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  extends: ['airbnb', 'plugin:prettier/recommended'],
+  extends: ['haixue-antd', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -19,15 +19,9 @@ module.exports = {
     strict: [0],
     'no-use-before-define': 1,
     'prettier/prettier': 2,
-    'import/no-unresolved': [2, { ignore: ['^@/', 'appconfig'] }],
-    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx'] }],
-    'react/prop-types': 1,
-    'react/prefer-stateless-function': 1,
-    'react/button-has-type': 1,
-    'react/state-in-constructor': [1, 'never'],
-    'react/jsx-fragments': [2, 'element'],
-    'react/jsx-one-expression-per-line': [0], // 与 prettier 规则冲突
-    'react/jsx-curly-brace-presence': [0], // 与 prettier 规则冲突
     // import/no-extraneous-dependencies 待定
+  },
+  settings: {
+    polyfills: ['Promise'],
   },
 };
