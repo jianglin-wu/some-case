af34aa34825883b843067acf93a054fafa194ec8
jianglin-wu <wjiangl.id@gmail.com>
2019-10-23 09:51:56 +0800

    perf: 配置优化'

--- src/service-worker.js
+++ src/service-worker.js
@@ -51,8 +51,9 @@ workbox.routing.setDefaultHandler(({ url, event }) => {
   event.respondWith(response);
 });
 
+// 带有 hash 文件名的采用缓存优先
 workbox.routing.registerRoute(
-  /https?:\/\/image-cdn.hahhub.com/,
+  /https?:\/\/image-cdn.xxxx.com/,
   new workbox.strategies.CacheFirst({
     plugins: [
       new workbox.cacheableResponse.Plugin({
@@ -62,14 +63,23 @@ workbox.routing.registerRoute(
   }),
   'GET',
 );
-workbox.routing.registerRoute(
-  /https?:\/\/blog-cdn.hahhub.com/,
-  new workbox.strategies.StaleWhileRevalidate(),
-  'GET',
-);
+
+// 普通静态文件先使用缓存在更新内容
 workbox.routing.registerRoute(
   /https?:\/\/dummyimage\.com/,
   new workbox.strategies.StaleWhileRevalidate(),
   'GET',
 );
-workbox.routing.registerRoute(/appconfig\.js$/, new workbox.strategies.NetworkFirst(), 'GET');
+
+// 以网络优先缓存动态注入的 appconfig.xxx.js 或 appconfig.js 文件
+// 注意： 启用了 Service Worker 并部署之后，再修改 Disconf 配置重新发布不会马上生效
+//   （因为 html 被缓存了，引入 appconfig.xxx.js 的地址没有更新）
+//
+// 解决方案一：需要重新打包生成了新的预缓存文件 hash 校验值部署之后才会更新。
+// 解决方案二：修改配置将生成的 appconfig.xxx.js 文件名固定为 appconfig.js，
+//           在 Nginx 服务器配置 appconfig.js 文件不被缓存（配置文件名固定之后如果有缓存则不会及时在客户端更新）。
+workbox.routing.registerRoute(
+  /appconfig\.[\w|-]{0,}\.?js$/,
+  new workbox.strategies.NetworkFirst(),
+  'GET',
+);
